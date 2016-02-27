ab37d222e970ad0b50589edfbeeb34da938b7474
Norio Miyake <miyake@MacBook-Pro15.local>
2016-02-27 21:34:35 +0900

    update setting.js

--- js/setting.js
+++ js/setting.js
@@ -5,12 +5,12 @@
 var Taxes = Taxes || {};
 
 Taxes.baseKoujo = 330000; // 住民税基礎控除
-Taxes.huyoKoujo = 660000; // 一人分の扶養控除
-Taxes.taxRate = 0.06; // 住民税率
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率(市民税のみ)
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'kurashikishi_2015_ippan';
+OpenSpending.identifier = 'niimi_2015_ippan';
 OpenSpending.year = '2015';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
@@ -18,17 +18,15 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1':  { icon: 'icons/legislative.svg',     color : '#C75746', b color : '#935B3B' },
-  '2':  { icon: 'icons/publicaffairs.svg',   color : '#C75746', b color : '#0AB971' },
-  '3':  { icon: 'icons/family.svg',          color : '#C75746', b color : '#4E6D00' },
-  '4':  { icon: 'icons/health.svg',          color : '#C75746', b color : '#D33673' },
-  '5':  { icon: 'icons/labour.svg',          color : '#C75746', b color : '#2A3A03' },
-  '6':  { icon: 'icons/pig.svg',             color : '#C75746', b color : '#EC2406' },
-  '7':  { icon: 'icons/money.svg',           color : '#C75746', b color : '#938626' },
-  '8':  { icon: 'icons/construction.svg',    color : '#C75746', b color : '#C75746' },
-  '9':  { icon: 'icons/fire-brigade.svg',    color : '#C75746', b color : '#D33673' },
-  '10': { icon: 'icons/education.svg',       color : '#C75746', b color : '#790586' },
-  '11': { icon: 'icons/economic-aid.svg',    color : '#C75746', b color : '#790586' },
-  '12': { icon: 'icons/civilian-action.svg', color : '#C75746', b color : '#790586' },
-  '13': { icon: 'icons/admin.svg',           color : '#C75746', b color : '#790586' }
+  '1': { icon: 'icons/family2.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#0AB971' },
+  '3': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '4': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#D33673' },
+  '5': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '6': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#EC2406' },
+  '7': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#938626' },
+  '8': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#C75746' },
+  '9': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#D33673' },
+  '10': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#790586' },
+  '11': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#790586' }
 };
